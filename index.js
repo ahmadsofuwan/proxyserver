a163731535cc9d473f74193728d86c1a2616d3a7
ahmadsofuwan <sofuwan62@gmail.com>
2025-01-17 20:42:22 +0700

    a

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 const socks = require('socksv5');
 
-// Konfigurasi Username dan Password
-const USERNAME = 'ahmadsofuwan';
-const PASSWORD = 'ahmadsofuwan123123';
-
-// Membuat server SOCKS5
+// Membuat server SOCKS4
 const server = socks.createServer((info, accept, deny) => {
   console.log(`Koneksi dari ${info.srcAddr}:${info.srcPort} ke ${info.dstAddr}:${info.dstPort}`);
   accept(); // Izinkan semua koneksi
 });
 
-// Autentikasi Username dan Password
-server.useAuth(socks.auth.UserPassword((user, password, callback) => {
-  if (user === USERNAME && password === PASSWORD) {
-    console.log(`Autentikasi sukses: ${user}`);
-    callback(true);
-  } else {
-    console.log(`Autentikasi gagal: ${user}`);
-    callback(false);
-  }
-}));
-
 // Jalankan server di port 1080
 server.listen(1212, '0.0.0.0', () => {
-  console.log('SOCKS5 proxy dengan autentikasi berjalan di port 1212');
+  console.log('SOCKS4 proxy berjalan di port 1212');
 });
 
 // Penanganan error
