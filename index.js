5c69fe13f14625d0389e0c534d6db3ff11799070
ahmadsofuwan <sofuwan62@gmail.com>
2025-01-17 21:27:08 +0700

    a

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const socks = require('socksv5');
 
-// Membuat server SOCKS4
-const server = socks.createServer((info, accept, deny) => {
+// Membuat server SOCKS4 dengan user dan password
+const server = socks.createServer({
+  authenticate: (username, password, socket, callback) => {
+    if (username === 'user' && password === 'password') {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  }
+}, (info, accept, deny) => {
   console.log(`Koneksi dari ${info.srcAddr}:${info.srcPort} ke ${info.dstAddr}:${info.dstPort}`);
   accept(); // Izinkan semua koneksi
 });
 
-// Jalankan server di port 1080
+// Jalankan server di port 1212
 server.listen(1212, '0.0.0.0', () => {
   console.log('SOCKS4 proxy berjalan di port 1212');
 });
